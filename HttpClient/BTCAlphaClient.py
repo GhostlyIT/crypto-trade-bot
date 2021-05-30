e0f1b9492279e3ca19df43716ee2e723189c923b
ghostlyit <supermaximal23@gmail.com>
2021-05-30 11:16:57 +0300

    Готов класс для работы с BTC Alpha

--- HttpClient/BTCAlphaClient.py
+++ HttpClient/BTCAlphaClient.py
@@ -6,8 +6,7 @@ from settings import settings
 
 
 class BTCAlphaClient:
-    @staticmethod
-    def __generate_headers(data):
+    def __generate_headers(self, data: dict) -> dict:
         msg = settings['exchange_api']['key'] + urlencode(sorted(data.items(), key=lambda val: val[0]))
 
         sign = hmac.new(settings['exchange_api']['secret'].encode(), msg.encode(), digestmod='sha256').hexdigest()
@@ -18,14 +17,62 @@ class BTCAlphaClient:
             'X-NONCE': str(int(time() * 1000)),
         }
 
-    def make_request(self, endpoint, method='get', data={}):
-        return requests.request(method, settings['exchange_api']['base_url'] + '/' + endpoint,
-                                headers=self.__generate_headers(data))
+    def __make_request(self, endpoint: str, method: str = 'get', data: dict = {}, params: dict = {}):
+        return requests.request(method,
+                                settings['exchange_api']['base_url'] + '/' + endpoint,
+                                headers=self.__generate_headers(data),
+                                params=params)
 
     def get_currencies(self):
-        return self.make_request('currencies')
+        return self.__make_request('currencies')
 
-    def get_wallet_info(self, currency=''):
-        return self.make_request('wallets?currency_id=' + currency)
+    def get_wallet_info(self, currency: str = ''):
+        return self.__make_request(endpoint='wallets', params={'currency_id': currency})
 
+    def get_created_orders(self, order_type: str = '', pair: str = '', status: str = '', limit: str = ''):
+        params = {
+            'type': order_type,
+            'pair': pair,
+            'status': status,
+            'limit': limit
+        }
+        return self.__make_request(endpoint='orders/own', params=params)
 
+    def get_order_info(self, order_id: int):
+        return self.__make_request(endpoint='order/' + str(order_id))
+
+    def create_order(self, order_type: str, pair: str, amount: float, price: float):
+        order = {
+            'type': order_type,
+            'pair': pair,
+            'amount': str(amount),
+            'price': str(price)
+        }
+        return self.__make_request(method='POST', endpoint='order', data=order)
+
+    def cancel_order(self, order_id: int):
+        return self.__make_request(method='POST', endpoint='order-cancel', data={'order': order_id})
+
+    def get_own_exchanges(self,
+                          order_type: str = '',
+                          pair: str = '',
+                          limit: int = 100,
+                          offset: int = 0,
+                          ordering: str = '-id'):
+        params = {
+            'type': order_type,
+            'pair': pair,
+            'limit': limit,
+            'offset': offset,
+            'ordering': ordering
+        }
+        return self.__make_request(endpoint='exchanges/own', params=params)
+
+    def get_charts(self, pair: str, timeline: str, limit: int = 720, since: int = None, until: int = None):
+        params = {
+            'limit': limit,
+            'since': since,
+            'until': until
+        }
+
+        return self.__make_request(endpoint='charts/' + pair + '/' + timeline + '/chart', params=params)
